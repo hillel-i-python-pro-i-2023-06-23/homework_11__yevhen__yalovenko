baa707c24f243f48217573896dc6e971ffd4568e
YevhenYalovenko <jenyaya0007@gmail.com>
2023-07-29 19:04:23 +0300

    Finish task with model. Start working on html

--- apps/contacts/models/contacts.py
+++ apps/contacts/models/contacts.py
@@ -1,10 +1,9 @@
 from django.db import models
-from phone_field import PhoneField
 
 
 class Contact(models.Model):
     name = models.CharField(max_length=100)
-    phone = PhoneField(blank=True, help_text="Contact phone number")
+    phone = models.CharField(max_length=13)
 
     created_at = models.DateTimeField(
         auto_now_add=True,
@@ -24,4 +23,4 @@ class Contact(models.Model):
     __repr__ = __str__
 
     class Meta:
-        ordering = ["modified_at"]
+        ordering = ["id", "name", "phone", "created_at", "modified_at"]
